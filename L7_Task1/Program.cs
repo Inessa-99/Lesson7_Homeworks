d17779047faf7a239c5ed17d06eb5f47ab17103d
Inessa <inesa-99@mail.ru>
2022-08-15 18:17:00 +0300

    Folder L7_Task1 ready

---
+++ L7_Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+double[,] GetArray(int row, int col)
+{
+    double[,] array = new double[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.NextDouble() * 100 - 50;
+            Console.Write(Math.Round(array[i,j], 1) + "\t");
+        }
+        Console.WriteLine();
+    }
+    return array;
+}
+
+int row = 3;
+int col = 4;
+
+GetArray(row, col);
